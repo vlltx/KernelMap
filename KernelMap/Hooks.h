4ebad14eaa7bac4cd612d58597836cd1b37b39af
vlltx <vlltx@tuta.io>
2025-08-28 16:49:03 +0300

    Add MmGetSystemRoutineAddressType

--- KernelMap/Hooks.h
+++ KernelMap/Hooks.h
@@ -8,5 +8,8 @@ EXTERN_C PVOID* FunctionTable;
 #define MM_GET_SYSTEM_ROUTINE_ADDRESS 0
 #define PS_GET_VERSION 1
 
+typedef PVOID (NTAPI* MmGetSystemRoutineAddressType)(PUNICODED_STRING RoutineName);
+
 PVOID KmMmGetSystemRoutineAddress(PUNICODE_STRING RoutineName);
+
 NTSTATUS KmPsGetVersion(PULONG MajorVersion, PULONG MinorVersion, PULONG BuildNumber, PUNICODE_STRING CSDVersion);
